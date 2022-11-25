37074de7316bec20b50b19beb1a71c6d0a8af891
kevinnail <fumalicious@gmail.com>
2022-11-24 18:45:30 -0800

    updated form labels

--- render-utils.js
+++ render-utils.js
@@ -5,19 +5,18 @@ export function renderCategoryOption(astroSign) {
     return option;
 }
 
-export function renderPost(post) {
+export function renderPost(post, user) {
     const li = document.createElement('li');
 
     const h3 = document.createElement('h3');
     h3.textContent = post.title;
     h3.classList.add('heading');
 
-    const h32 = document.createElement('h5');
-    h32.textContent = categoryEmoji(post.category);
-    h32.classList.add('category');
+    // const h32 = document.createElement('h5');
+    // h32.textContent = categoryEmoji(post.category);
+    // h32.classList.add('category');
 
     const img = document.createElement('img');
-
     img.classList.add('image-size');
     img.src = post.image_url;
 
@@ -30,24 +29,24 @@ export function renderPost(post) {
     return li;
 }
 
-function categoryEmoji(category) {
-    if (category === 'Beads') return 'Beads';
-    if (category === 'Blunt Tips') return 'Blunt Tips';
-    if (category === 'Collabs') return 'Collabs';
-    if (category === 'Cups') return 'Cups';
-    if (category === 'Dry Pieces') return 'Dry Pieces';
-    if (category === 'Goblets') return 'Goblets';
-    if (category === 'Iso Stations') return 'Iso Stations';
-    if (category === 'Marbles') return 'Marbles';
-    if (category === 'Pendants') return 'Pendants';
-    if (category === 'Bubblers') return 'Bubblers';
-    if (category === 'Recyclers') return 'Recyclers';
-    if (category === 'Rigs') return 'Rigs';
-    if (category === 'Slides') return 'Slides';
-    if (category === 'Spinner Caps') return 'Spinner Caps';
-    if (category === 'Misc') return 'Misc';
-    if (category === 'Terp Pearls') return 'Terp Pearls';
-}
+// function categoryEmoji(category) {
+//     if (category === 'Beads') return 'Beads';
+//     if (category === 'Blunt Tips') return 'Blunt Tips';
+//     if (category === 'Collabs') return 'Collabs';
+//     if (category === 'Cups') return 'Cups';
+//     if (category === 'Dry Pieces') return 'Dry Pieces';
+//     if (category === 'Goblets') return 'Goblets';
+//     if (category === 'Iso Stations') return 'Iso Stations';
+//     if (category === 'Marbles') return 'Marbles';
+//     if (category === 'Pendants') return 'Pendants';
+//     if (category === 'Bubblers') return 'Bubblers';
+//     if (category === 'Recyclers') return 'Recyclers';
+//     if (category === 'Rigs') return 'Rigs';
+//     if (category === 'Slides') return 'Slides';
+//     if (category === 'Spinner Caps') return 'Spinner Caps';
+//     if (category === 'Misc') return 'Misc';
+//     if (category === 'Terp Pearls') return 'Terp Pearls';
+// }
 function truncateString(str, n) {
     if (str.length > n) {
         return str.substring(0, n) + '...';
