0f816a273e9d0187c77fdeb8911bb2b87171ebfa
kevinnail <fumalicious@gmail.com>
2022-11-25 17:26:06 -0800

    add check for user on post page to show/ hide admin button

--- post/post.js
+++ post/post.js
@@ -1,5 +1,5 @@
 /*  Imports */
-import { getPost, getPosts, getUrls } from '../fetch-utils.js';
+import { getPost, getPosts, getUrls, getUser } from '../fetch-utils.js';
 
 /* DOM */
 const postTitle = document.getElementById('title');
@@ -7,20 +7,25 @@ const postDescription = document.getElementById('description');
 const postCategory = document.getElementById('category');
 const postPrice = document.getElementById('price');
 const gallery = document.getElementById('gallery');
-
+const postEditorLink = document.getElementById('post-editor-link');
 /*  State  */
 let error = null;
 let post = null;
 let urls = null;
+let user = getUser();
 
 /* Events */
 window.addEventListener('load', async () => {
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get('id');
     const response = await getPost(id);
-
     error = response.error;
     post = response.data;
+
+    if (!user) {
+        postEditorLink.textContent = '';
+    }
+
     if (error) {
         location.replace('/');
     } else {
